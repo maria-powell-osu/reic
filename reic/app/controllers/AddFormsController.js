fadb56cf070e449d6d11e7adf6c3a4e670681301
MariaPowell <mariapowelldesign@gmail.com>
2016-03-30 18:06:37 -0700

    added unit add form

--- reic/app/controllers/AddFormsController.js
+++ reic/app/controllers/AddFormsController.js
@@ -1,6 +1,15 @@
-App.controller('AddUnitController', ['$scope', function ($scope){
-	var ctrl = this;
-	var forms = [
-		
-	];
+App.controller('AddFormsController', ['$scope', function ($scope){
+	$scope.units = [];
+
+	$scope.units = [{id: 'unit1'}];
+
+	$scope.addUnit = function() {
+	    var newUnitNo = $scope.units.length + 1;
+	    $scope.units.push({'id':'unit'+ newUnitNo});
+  	};
+
+  	$scope.removeChoice = function() {
+    var lastItem = $scope.units.length-1;
+    $scope.units.splice(lastItem);
+  };
 }]);
