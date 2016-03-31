f3cc98b860c72fd7cea173c35382647dfd2b2caa
MariaPowell <mariapowelldesign@gmail.com>
2016-03-30 19:12:54 -0700

    added utlities add form

--- reic/app/controllers/AddFormsController.js
+++ reic/app/controllers/AddFormsController.js
@@ -1,5 +1,4 @@
-App.controller('AddFormsController', ['$scope', function ($scope){
-	$scope.units = [];
+App.controller('AddUnitController', ['$scope', function ($scope){
 
 	$scope.units = [{id: 'unit1'}];
 
@@ -8,8 +7,38 @@ App.controller('AddFormsController', ['$scope', function ($scope){
 	    $scope.units.push({'id':'unit'+ newUnitNo});
   	};
 
-  	$scope.removeChoice = function() {
+  	$scope.removeUnit = function() {
     var lastItem = $scope.units.length-1;
     $scope.units.splice(lastItem);
+  };
+}]);
+
+App.controller('AddSupplementalIncomeController', ['$scope', function ($scope){
+
+	$scope.incomes = [{id: 'income1'}];
+
+	$scope.addIncome = function() {
+	    var newNo = $scope.incomes.length + 1;
+	    $scope.incomes.push({'id':'income'+ newNo});
+  	};
+
+  	$scope.removeIncome = function() {
+    var lastItem = $scope.incomes.length-1;
+    $scope.incomes.splice(lastItem);
+  };
+}]);
+
+App.controller('AddUtilityController', ['$scope', function ($scope){
+
+	$scope.utilities = [];
+
+	$scope.addUtility = function() {
+	    var newNo = $scope.utilities.length + 1;
+	    $scope.utilities.push({'id':'income'+ newNo});
+  	};
+
+  	$scope.removeUtility = function() {
+    var lastItem = $scope.utilities.length-1;
+    $scope.utilities.splice(lastItem);
   };
 }]);
