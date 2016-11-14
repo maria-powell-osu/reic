f8d8b24f9dc23467675c14d87b43e6a1d17fa935
Maria <maria.powell.osu.com>
2016-11-13 17:03:24 -0800

    plan passive remodel phase 1

--- app/shared/navbar/navbar.js
+++ app/shared/navbar/navbar.js
@@ -1,9 +1,18 @@
-'use strict';
+	'use strict';
 
-//Navigation Bar: Need to Add Active Button Still
-App.directive('mpTopNavBar', function() {
-  return {
-  	restrict: 'E',
-    templateUrl: '/app/shared/navbar/navbar.html'
-  };
-});
+	//Navigation Bar: Need to Add Active Button Still
+	App.directive('mpTopNavBar', ['$location', function(location) {
+		return {
+			restrict: 'E',
+			templateUrl: '/app/shared/navbar/navbar.html',
+			link: function (scope,element, attrs, ctrl){
+				$('.navbar-collapse a').click(function(){
+					$(".navbar-collapse").collapse('hide');
+
+				});
+				scope.isActive = function (viewLocation) { 
+			        return viewLocation === location.path();
+			    };
+			}
+		};
+	}]);
